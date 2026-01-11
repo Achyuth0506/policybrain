0c58728fd0dee1e9af000a587e7a2a997ce244fd
Achyuth <undrakonda.ak@gmail.com>
2026-01-11 15:42:55 -0600

    Delete clinidoc directory

--- clinidoc/guidelines_data.py
+++
@@ -1,7 +0,0 @@
-GUIDELINES = [
-    "Chest pain with shortness of breath is a red flag",
-    "Family history of heart disease increases risk",
-    "ECG recommended for persistent chest pain",
-    "Immediate referral if symptoms worsen"
-]
-
